ee6d80ebabcd10200d78048cbd2926b28baab32e
Dayrell <dayrell2020@gmail.com>
2016-10-02 16:27:46 -0300

    Create articles table, model and add validations

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ActiveRecord::Base
+
+    #   validations
+    #   title validation: must have a title 
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+
+    #   description validation: must have a description
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+
+end
